490aac0e5fa38aa6e9029e302749e20f502e0a9f
Anderson Braga <anderson.braga@uniplaclages.edu.br>
2023-06-12 15:29:31 -0300

    Entidade Base

---
+++ BrinkFest/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrinkFest.WinApp.Compartilhado
+{
+    [Serializable]
+    public abstract class EntidadeBase<TEntidade>
+    {
+        public int id;
+
+        public abstract void AtualizarInformacoes(TEntidade registroAtualizado);
+
+        public abstract string[] Validar();
+    }
+}
