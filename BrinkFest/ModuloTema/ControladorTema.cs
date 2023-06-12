26f86b01b747a95e75895b6a074dd4e00340e62f
Arthur Leonardo Filipus <ArthurFilipus@hotmail.com>
2023-06-12 17:05:37 -0300

    Iniciado a configuração do Módulo Tema.

---
+++ BrinkFest/ModuloTema/ControladorTema.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrinkFest.WinApp.ModuloTema
+{
+    public class ControladorTema : ControladorBase
+    {
+        private RepositorioTema repositorioTema;
+        private ListagemTemaControl listagemTema;
+        public override string ToolTipInserir { get { return "Inserir novo Tema"; } }
+
+        public override string ToolTipEditar { get { return "Editar  Tema existente"; } }
+
+        public override string ToolTipExcluir { get { return "Excluir  Tema existente"; } }
+
+        public override void Inserir()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Editar()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Excluir()
+        {
+            throw new NotImplementedException();
+        }
+        public override UserControl ObterListagem()
+        {
+            if (listagemTema == null)
+                listagemTema = new ListagemTemaControl();
+
+            CarregarTema();
+
+            return listagemTema;
+        }
+        public override string ObterTipoCadastro()
+        {
+            return "Cadastro de Temas";
+        }
+        public void CarregarTema()
+        {
+            List<Tema> temas = repositorioTema.SelecionarTodos();
+            listagemTema.AtualizarRegistro(temas);
+        }
+    }
+}
---
+++ BrinkFest/ModuloTema/TelaTemaForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrinkFest.WinApp.ModuloTema
+{
+    public partial class TelaTemaForm : Form
+    {
+        public TelaTemaForm()
+        {
+            InitializeComponent();
+        }
+        private Tema tema;
+        public Tema Tema
+        {
+            get
+            {
+                return tema;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
